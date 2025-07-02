c5fe2c12f1688c7bf62b8ee4188d0caa2fe77f9f
teeratchapat <teeratchapat.c@kkumail.com>
2025-07-02 14:03:12 +0700

    feat: build error for project

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,8 @@ function App() {
     console.log('Hello My Project P_song');
   });
 
-  return (
     <>
-      <div>
+      <div> 
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
