9a933a43c815bc118b75da755e706be2a4b2e2b7
teeratchapat <teeratchapat.c@kkumail.com>
2025-07-02 14:02:15 +0700

    fix: update test commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    console.log('Hello My Project');
+    console.log('Hello My Project P_song');
   });
 
   return (
